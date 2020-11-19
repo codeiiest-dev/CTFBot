df4b3e840bc6729de2e7f7eaf7c62b43c314a40e
Mr-DareDevil <arnabsen1729@gmail.com>
2020-11-19 12:08:25 +0530

    CTF team registration added

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,16 @@ require("dotenv").config();
 const { Client, Message, MessageEmbed, Discord } = require("discord.js");
 const axios = require("axios");
 const client = new Client();
+const { teamRegister, getTeamId } = require('./db')
 
 const PREFIX = "!ctf ";
 const EVENT_URL = "https://ctftime.org/api/v1/events/?limit=";
+const TEAM_URL = "https://ctftime.org/api/v1/teams/";
 const HEADERS = {
   "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
 };
+
 
 // @TODO: Add duration
 getEvents = async (limit, channel) => {
@@ -19,7 +22,6 @@ getEvents = async (limit, channel) => {
       .then((resp) => {
         if (resp) {
           resp.data.forEach((ctf) => {
-            // console.log(ctf.title);
             const eventEmbed = new MessageEmbed()
               .setThumbnail(ctf.logo)
               .setTitle(ctf.title)
@@ -73,12 +75,51 @@ getEvents = async (limit, channel) => {
   }
 };
 
+getTeamInfo = async(teamId, channel) => {
+  console.log(teamId);
+  try {
+    const data = await axios
+      .get(`${TEAM_URL}${teamId}/`, { headers: HEADERS })
+      .then((resp) => {
+        if (resp) {
+            const teamData = resp.data;
+            console.log(teamData);
+            const ratingYear = Object.keys(teamData.rating[0])[0]
+            const rating = teamData.rating[0][ratingYear]
+            const eventEmbed = new MessageEmbed()
+              .setTitle(teamData.name)
+              .addField(
+                "ID: ",
+                teamData.id,
+                true
+              )
+              .addField(`Rating (${ratingYear})`, parseFloat(rating.rating_points).toFixed(2), true)
+              .addField(`Rank (${ratingYear})`, rating.rating_place, true)
+              .addField(
+                "Country: ",
+                teamData.country,
+                true
+              ).addField(
+                "Aliases: ",
+                teamData.aliases.slice(0, 2).join(' , '),
+                true
+              )
+              .setURL(`https://ctftime.org/team/${teamId}`)
+            channel.send(eventEmbed);
+        }
+      });
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+
 client.on("ready", () => {
   console.log(` ${client.user.tag} is logged in!`);
   client.user
     .setPresence({
       activity: {
-        name: "sadn1ck segfault | !ctf",
+        name: "!ctf help",
         type: "WATCHING",
         details: "(1001)",
       },
@@ -87,7 +128,7 @@ client.on("ready", () => {
     .catch(console.error);
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
   if (msg.content.startsWith(PREFIX)) {
     const [CMD, ...args] = msg.content
       .trim()
@@ -114,9 +155,29 @@ client.on("message", (msg) => {
     } else if (CMD.toLowerCase() === "help") {
       const helpEmbed = new MessageEmbed()
         .setTitle("Usage:\n!ctf <command> [...args]")
-        .addField("!ctf future n (n ∈ [1, 10]): ", "Displays n upcoming CTFs");
+        .addField("!ctf future n (n ∈ [1, 10]): ", "Displays n upcoming CTFs")
+        .addField("!ctf register <team-name> <team-id>: ", "Register team name with the CTFtime id")
+        .addField("!ctf showoff <team-name>: ", "Displays team details")
       channel.send(helpEmbed);
-    } else {
+    } else if (CMD.toLowerCase() === "register") {
+      if (args.length != 2) {
+        channel.send(PREFIX + "register <team-name> <team-id>");
+      } else {
+        const teamName = args[0];
+        const teamId = args[1];
+        teamRegister(teamName, teamId);
+        channel.send(`Team ${teamName} registered with ID ${teamId}`);
+      }
+    } else if (CMD.toLowerCase() === "showoff") {
+      if (args.length != 1) {
+        channel.send(PREFIX + "showoff <team-name>");
+      } else {
+        const teamName = args[0];
+        const teamId = await getTeamId(teamName);
+        getTeamInfo(teamId, channel);
+      }
+    }
+    else {
       channel.send("INVALID COMMAND (╯°□°）╯︵ ┻━┻");
     }
   }
